0c5de51e4efeb733df8fb197e152f68bf2c42cae
Han Jae Ho <gkdtjdrb95@gmail.com>
2022-03-18 18:14:23 +0900

    test

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, escape, redirect, request, render_template, url_for
+from werkzeug.utils import secure_filename
+
+# 가지고 있는 파일
+import applications.organizerTest as organizerTest
+
+app = Flask(__name__)
+
+#d ctrl + f5 시작
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/fileupload', methods=['GET', 'POST'])
+def file_upload():
+    file = request.files['file']
+    organizerTest.test(file)
+    return organizerTest.download_test(file) # 리턴해야 파일을 줌..
+
+if __name__=="__main__":
+    app.run()
+
---
+++ applications/organizerTest.py
@@ -0,0 +1,44 @@
+import os
+from time import sleep
+from flask import Flask, escape, request, render_template, send_file
+from werkzeug.utils import secure_filename
+import pandas as pd
+
+
+upload_path = './uploads/'
+
+def test(file):
+    filename = secure_filename(file.filename)
+    upload_test(file, filename)
+    analyze_txt(filename)
+
+
+# 파일 업로드
+def upload_test(file, filename):
+    print('test upload!!')
+    os.makedirs(upload_path, exist_ok=True)
+    file.save(os.path.join(upload_path, filename))
+
+# https://rfriend.tistory.com/250
+# txt 분석
+def analyze_txt(filename):
+    
+    print('analyze Test!!')
+    
+    df = pd.read_csv(f'{upload_path}{filename}', encoding='utf-8')
+    
+    print(df)
+    
+    df.to_excel(f'{upload_path}{filename}.xlsx',index=True)
+
+# 파일 다운로드
+def download_test(file):
+    print('download Test!!')
+    filename = secure_filename(file.filename) + '.xlsx'
+    return send_file(path_or_file = f'{upload_path}{filename}', 
+                                    attachment_filename = f'{filename}', 
+                                    as_attachment = True)
+
+
+
+
