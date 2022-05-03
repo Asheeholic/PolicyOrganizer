c7672a36b4a3560dbbd851542a4abcd2408ce657
Asheeholic <chacha_95@naver.com>
2022-05-03 19:21:07 +0900

    Error Solved

--- applications/analyzeTest.py
+++ applications/analyzeTest.py
@@ -13,6 +13,20 @@ def analyze_txt(filename, upload_path):
                     sep='------------------------------------------------------------', 
                     encoding='utf-8')
     
-    print(df.get)
+    print('Before Changing!')
+    print(df)
+
+    # Unnamed: 1 제거
+    df.drop(['Unnamed: 1'], axis = 1, inplace = True)
+    print('Changing 1')
+    print(df)
+
+    # Unnamed: 0 에서 처음부터 끝까지 분리 (for 또는 while)
+
+    # 분리 후 데이터를 다시 원하는 곳에 정렬하기 (DataFrame 다시 만들까? 아니면 있는걸 재정렬?)
     
-    df.to_excel(f'{upload_path}{filename}.xlsx',index=False, engine='openpyxl')
+    # 데이터 필요한 것 다듬기(데이터를 다시 계산해서 보이게 하기)
+
+    # 분리는 좀 있다가.
+    print('읽어보자')
+    df.to_excel(f'{upload_path}{filename}.xlsx',index=True)
--- applications/loadTest.py
+++ applications/loadTest.py
@@ -17,13 +17,13 @@ def upload_test(file, filename):
     os.makedirs(upload_path, exist_ok=True)
     file.save(os.path.join(upload_path, filename))
 
-# 파일 다운로드
-# def download_test(file):
-#     print('download Test!!')
-#     filename = secure_filename(file.filename) + '.xlsx'
-#     return send_file(path_or_file = f'{upload_path}{filename}', 
-#                                     attachment_filename = f'{filename}', 
-#                                     as_attachment = True)
+#파일 다운로드
+def download_test(file):
+    print('download Test!!')
+    filename = secure_filename(file.filename) + '.xlsx'
+    return send_file(path_or_file = f'{upload_path}{filename}', 
+                                    attachment_filename = f'{filename}', 
+                                    as_attachment = True)
 
 
 
