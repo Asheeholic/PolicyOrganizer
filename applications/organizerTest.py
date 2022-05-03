1bae9c343ff5184db9b687d212b81bea9603c4a3
Asheeholic <chacha_95@naver.com>
2022-05-03 17:56:47 +0900

    Planned and Unnamed removed

--- applications/organizerTest.py
+++ applications/organizerTest.py
@@ -1,3 +1,4 @@
+from calendar import c
 import os
 from time import sleep
 from flask import Flask, escape, request, render_template, send_file
@@ -28,9 +29,22 @@ def analyze_txt(filename):
     df = pd.read_csv(f'{upload_path}{filename}', 
                     sep='------------------------------------------------------------', 
                     encoding='utf-8')
-    
+    print('Before Change')
     print(df)
     
+    # Unnamed: 1 제거
+    df.drop(['Unnamed: 1'], axis = 1, inplace = True)
+    print('Changing 1')
+    print(df)
+
+    # Unnamed: 0 에서 처음부터 끝까지 분리 (for 또는 while)
+
+    # 분리 후 데이터를 다시 원하는 곳에 정렬하기 (DataFrame 다시 만들까? 아니면 있는걸 재정렬?)
+    
+    # 데이터 필요한 것 다듬기(데이터를 다시 계산해서 보이게 하기)
+
+    # 분리는 좀 있다가.
+
     df.to_excel(f'{upload_path}{filename}.xlsx',index=True)
 
 # 파일 다운로드
@@ -40,7 +54,3 @@ def download_test(file):
     return send_file(path_or_file = f'{upload_path}{filename}', 
                                     attachment_filename = f'{filename}', 
                                     as_attachment = True)
-
-
-
-
