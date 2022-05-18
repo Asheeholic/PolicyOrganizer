aea38a89281e875415be9e73e31575f22fa33e51
Asheeholic <chacha_95@naver.com>
2022-05-18 16:25:57 +0900

    제대로 된 작업인듯

--- testArea/analyze2.py
+++ testArea/analyze2.py
@@ -1,9 +1,9 @@
-from email import header
 import os
 from time import sleep
 from numpy import source
 from werkzeug.utils import secure_filename
 import pandas as pd
+import enums
 
 # https://rfriend.tistory.com/250
 # txt 분석
@@ -24,46 +24,82 @@ def analyze_txt(filename, upload_path):
     df_result = ''
     # 걍 천천히 할까? 뭐 나오면 뭐 넣어주고 (필요한것만)
 
-    subject = []
-    
-    # 요일
-    DAYS = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-    try:
-        for i in range(0, len(df.index) - 1) :
-            print()
-            # source_of_index = str(df.iloc[i, 0])
-            # print('source is {}'.format(source_of_index))
-            
-            # # 일단 ':' 로 나누고
-            # divided_of_source = source_of_index.split(":")
-
-            # if (divided_of_source not in subject) \
-            #     and (DAYS not in divided_of_source) :
-            #     subject.append(divided_of_source[0])
-
-            # if ':' in source_of_index :
-            #     divided_of_source = source_of_index.split(":")
-                
-            #     print(divided_of_source[0])
-
-            #     # 스케줄 또는 except 스케줄, include
-            #     if len(divided_of_source) > 2 :
-            #         print()        
-
-            #     if len(divided_of_source) == 2 :
-            #         df_result.iloc[i, 0] = divided_of_source[0]
-            #         df_result.iloc[i, 1] = divided_of_source[1]
-        
-
-    except:
-        print('Error Occur!!')
-    
+    subject = enums.SUBJECT
+    print('subject Test')
     print(subject)
     df_result = pd.DataFrame(columns=subject)
     
-    print('Changing 1')
-    print(df)
+    # try:
+    whileTurn = True
+    i = 0
+    while whileTurn :
+        # print('first while')
+        
+        source_of_index = str(df.iloc[i, 0])
+        
+        # Policy name check
+        if 'Policy Name:' not in source_of_index :
+            i += 1
+            continue
+
+        else :
+            if 'Policy Name:' in source_of_index :
+                divided_source = source_of_index.split(":")
+                policy_name = divided_source[1]
+                i += 1
+                
+                policy_type = ''
+                residence = '' # storage unit
+                volume_pool = ''
+                use_accelerator = ''
+                HW_OS_Client = ''
+                
+                while i < len(df.index):
+                    # print('second while')
+                    source_of_index = str(df.iloc[i, 0])
+                    divided_source = source_of_index.split(":")
+
+                    # print(i)
+                    # print(len(df.index))
+                    if divided_source[0] in subject :
+                        if 'Policy Type' in divided_source[0] :
+                            policy_type = divided_source[1]
+                        elif 'Residence' in divided_source[0] :
+                            residence = divided_source[1]
+                        elif 'Volume Pool' in divided_source[0] :
+                            volume_pool = divided_source[1]
+                        elif 'Use Accelerator' in divided_source[0] :
+                            use_accelerator = divided_source[1]
+                        # 수정
+                        elif 'HW/OS/Client' in divided_source[0] :
+                            HW_OS_Client = divided_source[1]
+                    
+                    if 'Policy Name' in str(df.iloc[i, 0]) :
+                        i = i - 1
+                        break
+                    
+                    i += 1
+                         
+                data = {
+                    subject[0] : policy_name,
+                    subject[1] : policy_type,
+                    subject[2] : residence,
+                    subject[3] : volume_pool,
+                    subject[4] : use_accelerator,
+                    subject[5] : HW_OS_Client,
+                }
+                # print(data)
+                df_result = df_result.append(data, ignore_index=True)
+                
+        
+        # 끝에 도달하거나 오류나면 끝내기
+        if i == len(df.index) :
+            break
+        
+        i += 1
+
+    # except:
+    #     print('Error Occur!!')
 
     print('result DataFrame')
     print(df_result)
@@ -77,5 +113,5 @@ def analyze_txt(filename, upload_path):
 # 다른 데이터 프레임을 만들고 거기에 넣는 방식으로 바꿔야 할 듯 하다.
 
 
-file = open('c:/git/PolicyOrganizer/testArea/policies.txt')
+file = open('c:/git/PolicyOrganizer/testArea/sogang5250_bppllist.txt')
 analyze_txt(file.name, '')
---
+++ testArea/enums.py
@@ -0,0 +1,25 @@
+DAYS = {
+    'Sunday' : ['Sunday', '일요일'], 
+    'Monday' : ['Monday', '월요일'], 
+    'Tuesday' : ['Tuesday', '화요일'],
+    'Wednesday' : ['Wednesday', '수요일'], 
+    'Thursday' : ['Thursday', '목요일'], 
+    'Friday' : ['Friday', '금요일'], 
+    'Saturday' : ['Saturday', '토요일']
+}
+
+SUBJECT = [
+    'Policy Name',
+    
+    'Policy Type',
+    'Residence', # storage unit
+    'Volume Pool',
+    'Use Accelerator',
+    'HW/OS/Client',
+    
+    'Include', # Backup Selection
+
+    'Schedule'
+
+
+]
